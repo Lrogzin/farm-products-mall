2cfc2a307d7f5d507cd71a7dc2f200cc41622f49
nuzoul <lrogzin@gmail.com>
2019-02-17 22:20:14 +0800

    init project

---
+++ mall-common/src/main/java/com/nuzoul/mall/common/service/BaseMapResp.java
@@ -0,0 +1,31 @@
+package com.nuzoul.mall.common.service;
+
+import java.util.HashMap;
+
+/**
+ * @Author: nuzoul
+ * @Date: 2019/2/9 18:40
+ */
+public class BaseMapResp extends HashMap<String, Object> {
+
+    public BaseMapResp() {
+        this.put("code", RespCode.FAIL);
+        this.put("message", RespMsg.SYSTEM_ERROR);
+    }
+
+    public void setCode(int code) {
+        this.put("code", code);
+    }
+
+    public void setMessage(String message) {
+        this.put("message", message);
+    }
+
+    public int getCode() {
+        return (int) this.get("code");
+    }
+
+    public String getMessage() {
+        return (String) this.get("message");
+    }
+}
