5bc886e77fbee12b5fbd3dd78215fd369791153b
nuzoul <lrogzin@gmail.com>
2019-02-17 23:33:46 +0800

    add config

---
+++ app-mall-admin/src/main/java/com/nuzoul/mall/common/PropertiesConfig.java
@@ -0,0 +1,32 @@
+package com.nuzoul.mall.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.util.Properties;
+/**
+ * @Author: nuzoul
+ * @Date: 2019/2/17 22:51
+ */
+public class PropertiesConfig {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private static final Properties properties = new Properties();
+    private static final PropertiesConfig INSTANCE = new PropertiesConfig();
+
+    private PropertiesConfig() {
+        try {
+            logger.info("PropertiesConfig:"+System.getProperty("cfg.env"));
+            properties.load(getClass().getClassLoader().getResourceAsStream("cfg_"+System.getProperty("cfg.env")+".properties"));
+        } catch (IOException e) {
+            logger.error("load config file recharge_config.properties exception", e);
+        }
+    }
+
+    public static PropertiesConfig getInstance() {
+        return INSTANCE;
+    }
+
+    private String get(String key) {
+        return properties.getProperty(key);
+    }
+}
